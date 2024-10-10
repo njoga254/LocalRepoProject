8a84697c445ed0df8d8c07dc7d3fea59141189f1
njoga lawrence <njogalawrence@gmail.com>
2024-10-10 17:23:58 +0300

    python programming dynamics

---
+++ robust_division_calculator.py
@@ -0,0 +1,14 @@
+def safe_divide(numerator, denominator):
+    try:
+        num1= float(numerator)
+        num2 = float(denominator)
+
+        result = num1 / num2
+        return f"The result of the division is {result}"
+
+    except ZeroDivisionError:
+        
+            return "Error: Cannot divide by zero"
+    except ValueError:
+         return "Error: Please enter numeric values only."
+         
